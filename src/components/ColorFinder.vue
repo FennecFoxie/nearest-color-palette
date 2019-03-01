3530ca4760e1e9693f95133936b5ecb99f53c275
AliceLevkovich <alice.levkovich@apalon.com>
2019-03-01 16:16:04 +0300

    Initial commit

---
+++ src/components/ColorFinder.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="color-finder">
+        <label for="">Hex color code (#000000) <input type="text" name="color-code"></label>
+        <button>Search</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ColorFinder", 
+    props: {
+
+    }
+}
+</script>
+
+<style lang="sass">
+
+</style>
